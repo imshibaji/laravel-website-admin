b03f8103fafaacf87e13965f71e5f531854f3bcd
Shibaji Debnath <imshibaji@gmail.com>
2020-10-01 00:28:12 +0530

    Command Uses fix

--- src/Console/Commands/AdminPub.php
+++ src/Console/Commands/AdminPub.php
@@ -39,43 +39,51 @@ class AdminPub extends Command
     public function handle()
     {
 
-        $q = $this->choice(
+        $optins = [
+            'Configuration File',
+            'Assets Files',
+            'Views Files',
+            'Databases Files',
+            'Factories Files',
+            'Seeds Files',
+            'Translate Files'
+        ];
+        $q = array_search($this->choice(
             "Which provider or tag's files would you like to publish?",
-            [
-                'Configuration File',
-                'Assets Files',
-                'Views Files',
-                'Databases Files',
-                'Factories Files',
-                'Seeds Files',
-                'Translate Files'
-            ]
-        );
-        if($q == 0){
-            Artisan::call('vendor:publish --tag=admin-config');
-            Artisan::call('config:clear');
-        }
-        if($q == 1){
-            Artisan::call('vendor:publish --tag=admin-assets');
-        }
-        if($q == 2){
-            Artisan::call('vendor:publish --tag=admin-views');
-            Artisan::call('views:clear');
-        }
-        if($q == 3){
-            Artisan::call('vendor:publish --tag=admin-migration');
-        }
-        if($q == 4){
-            Artisan::call('vendor:publish --tag=admin-factories');
-        }
-        if($q == 5){
-            Artisan::call('vendor:publish --tag=admin-seeds');
-        }
-        if($q == 6){
-            Artisan::call('vendor:publish --tag=admin-trans');
-        }
+            $optins), $optins);
 
-        $this->info('A: '. $q . ' published');
+            if($q == 0){
+                Artisan::call('vendor:publish --tag=admin-config');
+                Artisan::call('config:clear');
+                $this->info('Configuration File published');
+            }
+            else if($q == 1){
+                Artisan::call('vendor:publish --tag=admin-assets');
+                $this->info('Assets Files published');
+            }
+            else if($q == 2){
+                Artisan::call('vendor:publish --tag=admin-views');
+                Artisan::call('view:clear');
+                $this->info('Views Files published');
+            }
+            else if($q == 3){
+                Artisan::call('vendor:publish --tag=admin-migration');
+                $this->info('Databases Files published');
+            }
+            else if($q == 4){
+                Artisan::call('vendor:publish --tag=admin-factories');
+                $this->info('Factories Files published');
+            }
+            else if($q == 5){
+                Artisan::call('vendor:publish --tag=admin-seeds');
+                $this->info('Seeds Files published');
+            }
+            else if($q = 6){
+                Artisan::call('vendor:publish --tag=admin-trans');
+                $this->info('Translate Files published');
+            }
+
+
         return ;
     }
 }
