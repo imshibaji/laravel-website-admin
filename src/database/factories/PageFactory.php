349430e589ba06968b692451e9e4bf925bf3718a
Shibaji Debnath <imshibaji@gmail.com>
2020-09-30 19:25:32 +0530

    Add Page Factory

--- src/database/factories/PageFactory.php
+++ src/database/factories/PageFactory.php
@@ -2,13 +2,28 @@
 
 namespace Database\Factories;
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model\Page;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Shibaji\Admin\Models\Page;
 
-$factory->define(Page::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
+class PageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Page::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
