ffa740115b8d185434427c40eef1ad247f7dac6a
Shibaji Debnath <imshibaji@gmail.com>
2020-11-28 23:26:57 +0530

    User Model bug fixes

--- src/Models/User.php
+++ src/Models/User.php
@@ -5,13 +5,11 @@ namespace Shibaji\Admin\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasFactory;
     use Notifiable;
-    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
